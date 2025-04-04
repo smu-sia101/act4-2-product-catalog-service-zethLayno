f70a15bc2a237e641aac61c8b1f0be29970d78c3
ZETHLAURENCELAYNO <ZETHLAURENCELAYNO@SMU-0046>
2025-04-04 17:57:21 +0800

    implement Products API

---
+++ ProductCatalog_Layno/Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+namespace ProductCatalog_Layno.Models;
+
+  
+
+    public class ProductModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Name")]
+        public string ProductName { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public int Stock { get; set; } 
+        public string ImageUrl { get; set; } = null!;
+    }
